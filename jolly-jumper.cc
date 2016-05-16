ce2c1761b5f875e89a3469e67d9b011dea5481b3
Arshad Husain <husain3@cs.ualberta.ca>
2016-05-16 17:29:30 -0600

    finished the jolly jumper solution. Needs testing now

--- jolly-jumper.cc
+++ jolly-jumper.cc
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <cmath>
 
 int main()
 {
 	using namespace std;
 
 	int currArray[3000];
+	int elementDiff[3000];
 	FILE * fp;
 	fp = fopen("arrays.txt", "r");
 	if(fp != NULL)
@@ -15,16 +17,62 @@ int main()
 		while(!feof(fp))
 		{	
 			while(num != '\n')
-			{
+			{	
+				if(num != ' ')
+				{
+					int a = (int) num - '0';
+					currArray[iterator] = a;
+					iterator++;
+				}
 				fscanf(fp, "%c", &num);
-				int a = (int) num;
-				currArray[iterator] = a;
-				iterator++;
 			}
+			int elementDiffSize = 0;
+			for(int i = 0; i < iterator; i++)
+			{
+				//printf("%d ", currArray[i]);
+
+			}
+		//	printf("\n");	
+			for(int i = 0; i < iterator; i++ )
+			{
+				int diff = currArray[i] - currArray[i + 1];
+				
+				elementDiff[i] = abs(diff);
+				elementDiffSize = i;
+			
+			}
+			iterator = 0;
+			for(int i = 0; i < elementDiffSize; i++)
+			{
+      				//printf("%d ", elementDiff[i]);
+
+			}	
+			bool isJolly = true;
+			for(int j = 0; j < elementDiffSize-1; j++) 
+			{
+				int number = elementDiff[j];
+				
+				if(elementDiff[j+1] == number || elementDiff[j+1] == number-1)
+				{
+
+				} else {
+					isJolly = false;
+					break;
+				}
+			}
+			elementDiffSize = 0;
+			if(isJolly == true)
+			{
+				printf("Jolly\n");
+
+			} else {
+				printf("Not Jelly\n");
+			}
+			fscanf(fp, "%c", &num);		
 		}
 	} else {
-		printf("File cannot be opened\n");
-	
+		//printf("File cannot be opened\n");
+		return 0;
 	}
 
 
